8055924150fadc643a5fb009f85dce38b7312fb3
Shalom Crown <shalomcrown@gmail.com>
2025-04-05 20:52:11 +0300

    More fields

--- src/main/python/soccer/referee1.py
+++ src/main/python/soccer/referee1.py
@@ -24,19 +24,46 @@ sys.path.append(os.path.join(localDir, "../../protobuf"))
 import soccer_pb2 as sc
 import paho.mqtt.client as mqtt
 
+GAME_TOPIC = "soccer/game"
 
 # ===========================================================
 
 class Referee:
     def __init__(self):
         self.gameState = sc.GameState()
-        self.gameState.gameState = sc.GameStateType.NOT_STARTED
+        self.gameState.gameState = sc.GameStateType.WAITING_FOR_TEAMS
+
+        self.gameSetup = sc.GameSetup()
+        self.gameSetup.pitchCorners.extend([
+            sc.Vec3(x=0, y=0),
+            sc.Vec3(x=200, y=0),
+            sc.Vec3(x=200, y=100),
+            sc.Vec3(x=0, y=100),
+            ])
+        
+        self.gameSetup.playersPerTeam = 1
+        self.gameSetup.teamPlayersTotalMass = 1
+        self.gameSetup.teamPlayersTotalArea = 1
+        self.gameSetup.teamPlayersTotalPotentialEnergy = 1000;
+        self.gameSetup.coefficientOfFrictionPlayer = 0.3;
+        self.gameSetup.gameDurationSeconds = 90 * 60;
+
         
         self.mqclient = mqtt.Client(client_id="Refereee",
                      transport="TCP",
                      protocol=mqtt.MQTTv5)
+        self.mqclient.connect()
+
+        result = self.mqclient.publish(GAME_TOPIC, self.gameSetup.)
+
+    # ===========================================================================
+
+    def runGame(self):
+        pass
         
+
         
+
 
 # ===========================================================================
 
@@ -69,4 +96,6 @@ def setupLogging():
 
 if __name__ == '__main__':
     setupLogging();
+    ref = Referee()
+    ref.runGame()
 
